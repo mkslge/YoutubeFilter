1bedfae752f19a8938d048b492efbf7ccc39b54f
Mark Seeliger <marklseeliger@gmail.com>
2025-05-04 19:14:19 -0500

    go back to basic version rq

--- content.js
+++ content.js
@@ -1,19 +1,17 @@
+
 var neededKeywords = ["computer science", "software", "cs"];
-const RELOAD_TIME = 2000;
+const RELOAD_TIME = 10;
 function filterVideos() {
     const videos = document.querySelectorAll("ytd-rich-item-renderer");
 
     videos.forEach(video => {
 
-        let badgeArea = video.querySelector("ytd-badge-support", "ytd-desktop-meta-block");
-
-        let allText = video.innerText + (badgeArea?.innerText || "").toLowerCase();
         //turn video into title text format
-        let match = neededKeywords.some(keyword => allText.includes(keyword));
-
+        let title = video.innerText.toLowerCase();
         //if the video doesn't contain any keywords then dont display it
-        if(!match) {
+        if(!neededKeywords.some(keyword => title.includes(keyword))) {
             video.style.display = "none";
+
         }
     });
 }
